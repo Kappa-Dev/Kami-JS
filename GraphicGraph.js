aee038c364452b9e204e80796d6549766946c3fb
Adrien Basso-Blandin <cypher_dechu@hotmail.com>
2016-01-26 23:11:40 +0100

    stack added

--- GraphicGraph.js
+++ GraphicGraph.js
@@ -4,6 +4,7 @@
 //this file is part of the Executable Knowledge project
 //graphical graph version
 function GraphicGraph(containerid){//define a graphical graph with svg objects
+	var rewriter=[];
 	var containerID=containerid;
 	var layerG;//the layered graph data structure
 	var width;
@@ -15,16 +16,20 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 	var node_count=0;
 	var first_init;
 	var nugget_add,edition,kr_show,lcg_view,kappa_view;
+	var self=this;
 	this.wakeUp = function wakeUp(){//speed up tick function
 		//first_init=true;
 		for(var i=0;i<300;i++){
 			dynG.getForce().tick();
 		}
-	}
+	};
+	this.log = function log(){
+		layerG.log();
+	};
 	this.init = function init(){//init the graphic graph
 		nugget_add=false;
 		edition=false;
-		kr_show=false;
+		kr_show=true;
 		lcg_view=false;
 		kappa_view=false;
 		first_init=false;
@@ -33,7 +38,8 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 		height =document.getElementById(containerID).getBoundingClientRect().height-30;//menu is 30px heigth
 		svg=d3.select("#"+containerID).append("svg:svg")
 			.attr("width",width)
-			.attr("height",height);
+			.attr("height",height)
+			.on("contextmenu",d3.contextMenu(function(){return svgMenu();}));
 		dynG=new DynamicGraph(layerG,height,width);
 		dynG.init();
 		drag=dynG.getForce().drag()
@@ -49,7 +55,8 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 		s_link = svg.selectAll(".link")
 			.data(layerG.links, function(d) { return d.source.id + "-" + d.target.id; });
         s_link.enter().insert("line","g")
-            .classed("link",function(d){return d.e_class=="link"})
+			.classed("link",true)
+            .classed("links",function(d){return d.e_class=="link"})
 			.classed("parent",function(d){return d.e_class=="parent"});
         s_link.exit().remove();
 		//none action nodes svg representation
@@ -64,7 +71,7 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 			.classed("region",function(d){return d.classes[0]=="region"})
             .call(drag)
 			.on("mouseover",mouseOver)
-			.on("mouseout",mouseOut);
+			.on("mouseout",mouseOut)
 		nodeEnter.insert("circle")
 			.attr("r", function(d){return d.toInt()});
 		nodeEnter.insert("text")
@@ -73,7 +80,8 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 			.attr("dy", ".35em")
 			.attr("text-anchor", "middle")
 			.text(function(d) {if(d.label.length>0) return d.label[0]; else return d.id})
-			.attr("font-size", function(d){if(d.classes[0]!="action")return (d.toInt()/2)+"px"; else (d.toInt()/3)+"px";});
+			.attr("font-size", function(d){if(d.classes[0]!="action")return (d.toInt()/2)+"px"; else (d.toInt()/3)+"px";})
+			.on("dblclick",clickText);
 		s_node.exit().remove();
 		//action nodes svg representation
 		s_action = svg.selectAll("g.action")
@@ -95,7 +103,8 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 			.attr("dy", function(d){return d.toInt()/4;})
 			.attr("text-anchor", "middle")
 			.text(function(d) {if(d.label.length>0) return d.label[0]; else return d.id})
-			.attr("font-size", function(d){if(d.classes[0]!="action")return (d.toInt()/2)+"px"; else (d.toInt()/3)+"px";});
+			.attr("font-size", function(d){if(d.classes[0]!="action")return (d.toInt()/2)+"px"; else (d.toInt()/3)+"px";})
+			.on("dblclick",clickText);
 		s_action.exit().remove();
 		s_binder = svg.selectAll("circle.binder")
 			.data(function(){return layerG.nodes.filter(function(d){return d.classes[0]=="action" && d.classes[1]=="binder"})}, function(d) { return d.id;});
@@ -124,21 +133,25 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 			first_init=true;
 		}
 	};
-	var getNodeX = function(node){
+	var getNodeX = function(node){//return the x position on a specific node
 		//console.log(node);
 		if(node.classes[0]=="action" && node.classes[1]=="binder"){
-			if(node.classes[2]=="left")
+			if(node.classes[2]=="left" && node.father!=null)
 				return layerG.nodes[layerG.nodesHash[node.father]].x;
-			else if(node.classes[2]=="right")
+			else if(node.classes[2]=="right" && node.father!=null)
 				return layerG.nodes[layerG.nodesHash[node.father]].x+layerG.nodes[layerG.nodesHash[node.father]].toInt();
+		}else if(node.classes[0]=="action"){
+			return node.x+node.toInt()/2
 		}else{ 
 			return node.x;
 		}
 	};
-	var getNodeY = function(node){
+	var getNodeY = function(node){//return the y position on a specific node
 		//console.log(node);
-		if(node.classes[0]=="action" && node.classes[1]=="binder")
+		if(node.classes[0]=="action" && node.classes[1]=="binder" && node.father!=null)
 			return layerG.nodes[layerG.nodesHash[node.father]].y+layerG.nodes[layerG.nodesHash[node.father]].toInt()/4;
+		else if(node.classes[0]=="action")
+			return node.y+node.toInt()/4
 		else
 			return node.y;
 	};
@@ -148,7 +161,9 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 	this.addNode = function addNode(classes,label,path,x,y){//add a new node in the svg AND in the graph structure
 		dynG.getForce().stop();
 		layerG.addNode(classes,"n"+node_count,x,y,(typeof(x)!='undefined' && typeof(y)!='undefined'));
+		stack(layerG,layerG.removeNode,["n"+node_count]);
 		layerG.addLabel("n"+node_count,label);
+		stack(layerG,layerG.rmLabel,["n"+node_count,label]);
 		for(var i=path.length-1;i>0;i--){
 			if(typeof(layerG.nodesHash[path[i]])=='undefined'){
 				console.log("undefined path : "+path[i]+"for n"+node_count);
@@ -158,8 +173,10 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 				console.log(path);
 				return;
 			}				
-		}if(path.length>0)
+		}if(path.length>0){
 			layerG.setFather("n"+node_count,path[path.length-1]);
+			stack(layerG,layerG.removeParenting,["n"+node_count]);
+		}
 		node_count++;
 		update();
 		//dynG.getForce().on("tick",tick);
@@ -167,8 +184,27 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 	};
 	this.mergeNode = function mergeNode(new_nodeID,old_nodeID){//mergeDIFF both 
 		dynG.getForce().stop();
-		if(layerG.nodes[layerG.nodesHash[new_nodeID]].classes.join('_') == layerG.nodes[layerG.nodesHash[old_nodeID]].classes.join('_') /*&& layerG.getPath(new_nodeID).join('_') == layerG.getPath(old_nodeID).join('_')*/)
-			layerG.mergeDiff(new_nodeID,old_nodeID);
+		if(layerG.nodes[layerG.nodesHash[new_nodeID]].classes.join('_') == layerG.nodes[layerG.nodesHash[old_nodeID]].classes.join('_') /*&& layerG.getPath(new_nodeID).join('_') == layerG.getPath(old_nodeID).join('_')*/){
+			/*stacking modifications */
+			for(var i=0;i<layerG.links.length;i++){
+				if(links[i].sourceID==new_nodeID || links[i].sourceID==old_nodeID || links[i].targetID==new_nodeID || links[i].targetID==old_nodeID)
+					stack(layerG,layerG.addEdge,[links[i].sourceID,links[i].targetID]);//add all the links
+			}for(var i=0;i<layerG.nodes[new_nodeID].sons.length;i++)
+				stack(layerG,layerG.setFather,[layerG.nodes[new_nodeID].sons[i],new_nodeID]);//add all the sons
+			for(var i=0;i<layerG.nodes[old_nodeID].sons.length;i++)
+				stack(layerG,layerG.setFather,[layerG.nodes[old_nodeID].sons[i],old_nodeID]);
+			stack(layerG,layerG.setFather,[new_nodeID,layerG.nodes[new_nodeID].father]);//put fathers
+			stack(layerG,layerG.setFather,[old_nodeID,layerG.nodes[old_nodeID].father]);
+			stack(layerG,layerG.addLabel,[old_nodeID,layerG.nodes[old_nodeID].label.concat()]);//put label and context
+			stack(layerG,layerG.addCtx,[old_nodeID,layerG.nodes[old_nodeID].context.concat()]);
+			stack(layerG,layerG.addLabel,[new_nodeID,layerG.nodes[new_nodeID].label.concat()]);
+			stack(layerG,layerG.addCtx,[new_nodeID,layerG.nodes[new_nodeID].context.concat()]);
+			stack(layerG,layerG.addNode,[layerG.nodes[new_nodeID].classes,new_nodeID]);//add the old and new nodes
+			stack(layerG,layerG.addNode,[layerG.nodes[old_nodeID].classes,old_nodeID]);
+			stack(layerG,layerG.removeNode,[old_nodeID]);//remove the merged node
+			/* end stack -> modification */
+			layerG.mergeDiff(new_nodeID,old_nodeID);		
+		}
 		else{
 			console.log("unable to merge too different nodes : ");
 			console.log(layerG.nodes[layerG.nodesHash[new_nodeID]]);
@@ -178,36 +214,51 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 	};
 	this.removeNode = function removeNode(nID){//remove a node from the svg AND the graph structure
 		dynG.getForce().stop();
+		for(var i=0;i<layerG.links.length;i++){
+			if(links[i].sourceID==nID || links[i].targetID==nID)
+				stack(layerG,layerG.addEdge,[links[i].sourceID,links[i].targetID]);//add all the links
+		}for(var i=0;i<layerG.nodes[nID].sons.length;i++)
+			stack(layerG,layerG.setFather,[layerG.nodes[nID].sons[i],nID]);//add all the sons
+		stack(layerG,layerG.setFather,[nID,layerG.nodes[nID].father]);
+		stack(layerG,layerG.addLabel,[nID,layerG.nodes[nID].label.concat()]);
+		stack(layerG,layerG.addCtx,[nID,layerG.nodes[nID].context.concat()]);
+		stack(layerG,layerG.addNode,[layerG.nodes[nID].classes,nID]);
 		layerG.removeNode(nID);
 		update();			
 	};
 	this.addEdge = function addEdge(id1,id2){//add a LINK edge between two nodes in the svg AND in the graph structure
 		dynG.getForce().stop();
 		layerG.addEdge(id1,id2);
+		stack(layerG,layerG.removeEdge,[id1,id2]);
 		update();	
 	};
 	this.removeEdge = function removeEdge(id1,id2){//remove a LINK edge between two nodes in the svg AND in the graph structure
 		dynG.getForce().stop();
+		stack(layerG,layerG.addEdge,[id1,id2]);
 		layerG.removeEdge(id1,id2);
 		update();		
 	};
 	this.addParent = function addParent(son,fath){//add a PARENT edge between two node in the graph structure and un the svg and update graph structure
 		dynG.getForce().stop();
+		stack(layerG,layerG.removeParenting,[son]);
 		layerG.setFather(son,fath);
 		update();	
 	};
 	this.rmParent = function rmParent(son){//idem for removing parenting
 		dynG.getForce().stop();
+		stack(layerG,layerG.setFather,[son,layerG.nodes[son].father]);
 		layerG.removeParenting(son);
 		update();	
 	};
 	this.addCtx = function addCtx(id,ctx){//add a context to a specific node
 		dynG.getForce().stop();
+		stack(layerG,layerG.rmCtx,[id,ctx]);
 		layerG.addCtx(id,ctx);
 		update();	
 	};
 	this.rmCtx = function rmCtx(id,ctx){//remove a context from a specific node
 		dynG.getForce().stop();
+		stack(layerG,layerG.addCtx,[id,layerG.nodes[id].context.concat()]);
 		layerG.rmCtx(id,ctx);
 		update();	
 	};
@@ -216,11 +267,13 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 	};
 	this.addLabel = function addLabel(id,lbl){//add a label to a specific node
 		dynG.getForce().stop();
+		stack(layerG,layerG.rmLabel,[id,lbl]);
 		layerG.addLabel(id,lbl);
 		update();	
 	};
 	this.rmLabel = function rmLabel(id,lbl){//remove a label from a specific node
 		dynG.getForce().stop();
+		stack(layerG,layerG.addLabel,[id,layerG.nodes[id].label.concat()]);
 		layerG.rmLabel(id,lbl);
 		update();	
 	};
@@ -238,6 +291,23 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 			ret.push({x:layerG.nodes[i].x,y:layerG.nodes[i].y});
 		}
 		return ret;	
+	};
+	var stack = function(obj,fun,param){
+		rewriter.push({f:fun,o:obj,p:param});
+	}
+	this.unStack = function unStack(){
+		//layerG.log();
+		if(rewriter.length==0){
+			console.log("stack empty");
+			return;
+		}
+		fun=rewriter.pop();
+		console.log(fun);
+		fun.f.apply(fun.o,fun.p);
+		update();
+	};
+	this.clearStack = function clearStack(){
+		rewriter=[];
 	};
 	this.setState = function setState(state){//set the graphical interface state : "nugget_view", "kr_view", "kr_edit", "lcg_view", "kappa_view"
 		switch(state){
@@ -276,7 +346,7 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 				lcg_view=false;
 				kappa_view=true;
 				break;
-		}
+		}console.log(state);
 	};
 	this.getState = function getState(state){//get the graphical interface state
 		if(nugget_add) return "nugget_view";
@@ -287,11 +357,14 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 	};
 	var mouseOver = function(d){//handling mouse over nodes/actions
 		if(d3.select(this).classed("node")){
-			var div_ct="<p><h3>id: "+d.id+"</b><br/>labels: "+d.label.join("<br/>")+"context: "+d.context.join("<br/>")+"</p>" ;
+			var div_ct="<p><h3><b><center>id: "+d.id+"</center></b>";
+			if(d.label.length>0)
+				div_ct+="\n labels: <ul style='padding: 3px 15px;margin:1px;'><li>"+d.label.join("<li>")+"</ul>";
+			if(d.context.length>0)
+				div_ct+="values: <ul style='padding: 7px;margin:1px;'><li>"+d.context.join("<li>")+"</ul>";
+			div_ct+="</p>";
 			d3.select(".n_tooltip").style("visibility","visible")
 								.html(div_ct);
-			//console.log(d.id+"\n"+d.label.join(",")+"\n"+d.context.join(","));
-			//console.log(layerG.nodes[layerG.nodesHash[d.id]]);
 		}
 	};
 	var mouseOut = function(d){//handling mouse out of nodes/actions
@@ -306,6 +379,65 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 		}else{
 			
 		}
+	};
+	var svgMenu = function(){
+		var menu;
+		//console.log("here");
+		if(!(edition || nugget_add)){
+			menu = [
+			{
+				title: "Unlock all",
+				action: function(elm,d,i){
+					d3.selectAll("g").filter(function(d){return d.classes[0]=="agent" || d.classes[0]=="action"}).classed("fixed",function(d){return d.fixed=false;});
+					update();
+				}
+			}
+			];
+		}else{
+		//console.log("triggered !!!");
+		menu =[
+			{
+				title: "Unlock all",
+				action: function(elm,d,i){
+					d3.selectAll("g").filter(function(d){return d.classes[0]=="agent" || d.classes[0]=="action"}).classed("fixed",function(d){return d.fixed=false;});
+					update();
+				}
+			},{
+				title: "Select all",
+				action: function(elm,d,i){
+					d3.selectAll("g").filter(function(d){return d.classes[0]!="action" || d.classes[1]!="binder"}).classed("selected",function(d){return d.selected=true;});
+					update();
+				}
+			},{
+				title: "Unselect all",
+				action: function(elm,d,i){
+					d3.selectAll("g").filter(function(d){return d.classes[0]!="action" || d.classes[1]!="binder"}).classed("selected",function(d){return d.selected=false;});
+					update();
+				}
+			},{
+				title: "Add Agent",
+				action: function(elm,d,i){
+					var mousepos=d3.mouse(svg[0][0]);
+					var lbl=window.prompt("define your label","");
+					if(lbl=="")self.addNode(["agent"],[],[],mousepos[0],mousepos[1]);
+					else self.addNode(["agent"],[lbl.split(",")],[],mousepos[0],mousepos[1]);
+				}
+					
+			},{
+				title: "Add Action",
+				action: function(elm,d,i){
+					var mousepos=d3.mouse(svg[0][0]);
+					var act_t="bnd";
+					self.addNode(["action",act_t],[],[],mousepos[0],mousepos[1]);
+					//console.log(layerG.nodes[layerG.nodes.length-1]);
+					var last_node=layerG.nodes[layerG.nodes.length-1].id
+					self.addNode(["action","binder","left"],[],[last_node]);
+					self.addNode(["action","binder","right"],[],[last_node]);
+				}
+			}
+		];
+		}
+		return menu;
 	};
 	var dblClick = function(d){//handling double clik on nodes/action : edition
 		d3.event.stopPropagation();
@@ -322,6 +454,48 @@ function GraphicGraph(containerid){//define a graphical graph with svg objects
 				d3.select(this).classed("selected", d.selected = true);
 		}	
 	};
+	var clickText = function(d){//click on labels
+		if(!edition) return;
+        var el = d3.select(this);
+        var frm = svg.append("foreignObject");
+        var inp = frm
+            .attr("x", getNodeX(d)-100)
+            .attr("y", getNodeY(d)-12)
+            .attr("width", 200)
+            .attr("height", 25)
+            .append("xhtml:form")
+                    .append("input")
+                        .attr("value", function() {
+                            this.focus();
+                            return d.label.join(",");
+                        })
+                        .attr("style", "width: 294px;")
+                        .on("blur", function() {//change node label on focus lost : may be removed
+                            var txt = inp.node().value;
+							layerG.rmLabel(d.id,[]);
+							if(txt!=null && txt!="")
+								layerG.addLabel(d.id,txt.split(","));
+                            svg.select("foreignObject").remove();
+							el.text(function(d) {if(d.label.length>0) return d.label[0]; else return d.id});
+                        })
+                        .on("keypress", function() {
+                            var e = d3.event;
+                            if (e.keyCode == 13){
+                                if (e.stopPropagation)
+                                  e.stopPropagation();
+								  e.preventDefault();
+								var txt = inp.node().value;
+                                layerG.rmLabel(d.id,[]);
+								if(txt!=null && txt!="")
+									layerG.addLabel(d.id,txt.split(","));
+                                svg.select("foreignObject").remove();
+								el.text(function(d) {if(d.label.length>0) return d.label[0]; else return d.id});
+                            }
+                        });
+	}
+	var nodeStaticMenu = function(){
+		
+	}
 };
 
 //example
@@ -354,5 +528,6 @@ window.onload = function () {
 	gGraph.addEdge("n4","n18");
 	gGraph.addCtx("n3",["n1","n4"]);
 	gGraph.wakeUp();
+	//gGraph.log();
 	//console.log(gGraph.getCoord());
 };
