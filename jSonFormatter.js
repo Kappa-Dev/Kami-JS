10bd2a7445cdb4c9a63e468dcc84770c9fb40af5
Adrien Basso-Blandin <cypher_dechu@hotmail.com>
2016-03-15 20:59:07 +0100

    old format

--- jSonFormatter.js
+++ jSonFormatter.js
@@ -8,6 +8,15 @@ function jSonFormatter(gGraph){
 	var gGraph=gGraph;
 	var force=false;
 	//var count=-1;
+	var sublist = function(list,start,end){//return a new list containing the value from start to end(excluded)
+		var ret=[];
+		if(start==null) start =0;
+		if(end==null) end=list.length;
+		for(var i=start;i<end && i<list.length;i++){
+			ret.push(list[i]);
+		}
+		return ret;
+	}
 	this.init = function init(filename){//import a layered graph from a json file
 		name=filename;
 		count=gGraph.lastNode();
@@ -25,21 +34,34 @@ function jSonFormatter(gGraph){
 				edges:graph.edges
 			}
 			jsToLGraph();
+			console.log(json);
 		});	
-	}
+	};
 	var jsToLGraph = function(){//translate json to graph
 		force=false;
 		if(typeof(json.version)=='undefined' || json.version==null || json.version<1.1)
 			force=true; oldCast();
-		console.log(json);
+		//console.log(json);
 		var keys = Object.keys(json);
 		for(var i=0;i<keys.length;i++){
 			if(typeof(json[keys[i]])!='undefined' && json[keys[i]]!=null && json[keys[i]].length>0){
-				if(json[keys[i]]=="edges") genEdges(json[keys[i]]);
-				else genNode_Rec(json[keys[i]],keys[i]);
+				for(var j=0;j<json[keys[i]].length;j++){
+					if(keys[i]=="edges") genEdges(json[keys[i]][j]);
+					else genNode_Rec(json[keys[i]][j],keys[i]);
+				}
 			}else if(force && keys[i]!="version"){
 				console.log("Unable to find"+keys[i]+" !");
 				return;
+			}
+		}
+	};
+	var fndNCvtThatF_ingAction = function(act_name){
+		for(var i=0;i<json.actions.length;i++){
+			if(json.actions[i].labels[0]==act_name)
+				return classCast(json.actions[i].classes);
+			else{
+				console.log("unable to find this action ! : "+act_name);
+				return null;
 			}
 		}
 	}
@@ -50,66 +72,108 @@ function jSonFormatter(gGraph){
 			for(var j=0;j<json[keys[i]].length;j++){
 				var elt=json[keys[i]][j];
 				switch(keys[i]){
-					case "agents":     json[keys[i]][j]={classes:["agent"],labels:[elt.name],path:[],path_cl:null,values:null,x:elt.cx,y:elt.cy};break;
-					case "regions":    json[keys[i]][j]= {classes:["region"],labels:[elt.name],path:[elt.ag_name],path_cl:"agent",values:null};break;
+					case "agents":     json[keys[i]][j]={classes:["agent"],labels:[elt.name],path:[],path_cl:[],values:null,x:elt.cx,y:elt.cy};break;
+					case "regions":    json[keys[i]][j]= {classes:["region"],labels:[elt.name],path:[elt.ag_name],path_cl:["agent"],values:null};break;
 					case "key_rs":
-						if(elt.region_name!=null) json[keys[i]][j]= {classes:["key_res"],labels:[elt.name],path:[elt.region_name,elt.ag_name],path_cl:"region",values:null};
-						else json[keys[i]][j]= {classes:["key_res"],labels:[elt.name],path:[elt.ag_name],path_cl:"agent",values:null};
+						if(elt.region_name!=null) json[keys[i]][j]= {classes:["key_res"],labels:[elt.name],path:[elt.region_name,elt.ag_name],path_cl:["region"],values:null};
+						else json[keys[i]][j]= {classes:["key_res"],labels:[elt.name],path:[elt.ag_name],path_cl:["agent"],values:null};
 						break;
-					case "attributes": json[keys[i]][j]= {classes:["attribute","list"],labels:[elt.name],path:elt.dest_path,path_cl:classCast(elt.dest_class[1]),values:elt.values};break;
-					case "flags":      json[keys[i]][j]={classes:["flag"],labels:[elt.name],path:elt.dest_path,path_cl:classCast(elt.dest_class[1]),values:elt.values};break;
+					case "attributes": json[keys[i]][j]= {classes:["attribute","list"],labels:[elt.name],path:elt.dest_path,path_cl:classCast(elt.dest_class),values:elt.values};break;
+					case "flags":      json[keys[i]][j]={classes:["flag"],labels:[elt.name],path:elt.dest_path,path_cl:classCast(elt.dest_class),values:elt.values};break;
 					case "actions":
-						if(elt['class'][2]=="mod") json[keys[i]][j]= {classes:["action",classCast(elt['class'][2]),setMod(elt.mods)],labels:[elt.name],path:[],path_cl:null,context:castCtx(elt.context)};
-						else json[keys[i]][j]= {classes:["action",classCast(elt['class'][2])],labels:[elt.name],path:[],path_cl:null,context:castCtx(elt.context)};
+						if(elt['class'][2]=="mod") json[keys[i]][j]= {classes:["action","mod",setMod(elt.mods)],labels:[elt.name],path:[],path_cl:[],context:castCtx(elt.context)};
+						else json[keys[i]][j]= {classes:classCast(elt['class']),labels:[elt.name],path:[],path_cl:null,context:castCtx(elt.context)};
 						break;
-					case "actions_binder": json[keys[i]][j]= {classes:["action","binder",elt.name],labels:[],path:[elt.act_name],path_cl:"action",values:null};break;
-					case "edges": json[keys[i]][j]= {in_class:classCast(elt.in_class[1]),in_path:elt.in_path,out_class:classCast(elt.out_class[1]),out_path:elt.out_path};break;
+					case "actions_binder": 
+						json[keys[i]][j]= {classes:["action","binder",elt.name],labels:[],path:[elt.act_name],path_cl:fndNCvtThatF_ingAction(elt.act_name),values:null};break;
+					case "edges": json[keys[i]][j]= {in_class:classCast(elt.in_class),in_path:elt.in_path,out_class:classCast(elt.out_class),out_path:elt.out_path};break;
 					default: console.log("unknown key class : "+keys[i]);
 				}
 			}
 		}
 	}
-	var classCast = function(cls){
-		switch(cls){
-			case "key_r":
-			case "key_rs":
-			return "key_res";
-			case "bind":
-			return "bnd";
-			case "mod":
-			return "mod";
-			case "attr":
-			return "attribute";
-			default:
-			return cls;
+	var findClass = function(length,pos,orig_cl,dest_cl){
+		switch(orig_cl[0]){
+			
+			case "agents":
+				console.log("an agent can't be a son");
+				return null;
+			case "actions":
+				console.log("an action can't be a son");
+				return null;
+			case "regions":
+				if(dest_cl[0]!="agent") console.log("a region can't be the son of a none agent node");
+				return ["agent"];
+			case "actions_binder":
+				if(dest_cl[0]!="action")
+				return dest_cl;
+				
 		}
 	}
+	var classCast = function(cls){//translate old class format to the new one
+		var tmp=[];
+		for(var i=0;i<cls.length;i++){
+			switch(cls[i]){
+				case "node":
+				case "edge":
+					break;
+				case "key_r":
+				case "key_rs":
+					tmp.push("key_res");
+					break;
+				case "bind":
+					tmp.push("bnd");
+					break;
+				case "mod":
+					tmp.push("mod");
+					break;
+				case "attr":
+					tmp.push("attribute");
+					break;
+				default:
+					tmp.push(cls[i]);
+			}
+		}
+		return tmp;
+	};
+	var setMod = function(val){//translate old mod to the new version
+		if(val=="incr") return "pos";
+		else return "neg";
+	};
 	var genEdges = function (js_edges){
 		//console.log(js_edges);
 	}
-	var genNode_Rec = function(js_nodes,js_class){
-		//console.log(js_class);
-		//console.log(js_nodes);
+	var genNode_Rec = function(js_node,js_class){
+		console.log(js_class);
+		console.log(js_node);
+		/*for(var i=0;i<js_node.path.length;i++){
+			var tmp_node=gGraph.findByName([js_node.path[i]],findClass(js_node.path.length,i,js_node.classes,js_node.path_cl),sublist(js_node.path,0,js_node.path.length-1));
+			console.log(js_node.path[i]);
+			console.log(findClass(js_node.path.length-i,js_class,js_node.path_cl));
+			console.log(sublist(js_node.path,0,js_node.path.length-1));
+			console.log(tmp_node);
+		}*/
+			//gGraph.addNode()
+		if(js_class!="action"){
+			
+		}
 	}
 
 
 	
-	var setMod = function(val){
-		if(val=="incr") return "pos";
-		else return "neg";
-	}
-	var castCtx = function(ctx){
+	
+	var castCtx = function(ctx){//translate the old context to the new one.
 		var ret=[];
 		for(var i=0;i<ctx.length;i++){
 			var tmp_label=[ctx[i].el_path[ctx[i].el_path.length-1]];
 			ctx[i].el_path.pop();
 			var tmp_path=ctx[i].el_path;
-			var tmp_path_cl=classCast(ctx[i].el_cl[1]);
+			var tmp_path_cl=classCast(ctx[i].el_cl);
 			var tmp_values=ctx[i].el_value;
 			ret.push({labels:tmp_label,path:tmp_path,path_cl:tmp_path_cl,values:tmp_values});
 		}
 		return ret;
-	}
+	};
 
 	
 	var checkpath = function(lgraph,path){
