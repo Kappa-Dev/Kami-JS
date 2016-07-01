c893e8eea283af7c47c8578b8f238464bb0bd28b
Adrien Basso-Blandin <cypher_dechu@hotmail.com>
2016-07-01 09:25:03 +0200

    update

--- Kami.js
+++ Kami.js
@@ -76,7 +76,7 @@ function Node(i,t,n,l,v,u){
 	var id=i;//unique identifier of a node
 	if(typeof t=='undefined' || t==null || t.length==0)
 		throw "unknown type :"+t;
-	var type=t;//this node type : component(1)/action(2)/super(3)/attribute (1):agent/region/keyres/flag (2):modPose/modNeg/syn/deg/bnd/brk/input/output (3):family/set/process
+	var type=t;//this node type : component(1)/action(2)/super(3)/attribute (1):agent/region/keyres/flag (2):modPos/modNeg/syn/deg/bnd/brk/input/output (3):family/set/process
 	var labels=l || [];
 	var values=v || [];
 	var uid=u || null;
@@ -547,8 +547,216 @@ function UndoRedoStack(){//generic implementation of undo redo as graph rw rules
 		console.log(sub_stack);
 	}
 }
-
-function Kami(container){
+function graphicGui(s,n,e,k,c){//a graphic Gui use a svg caneva, nodes and edges and listener from the kami object.
+	var nodes=n || [];
+	var nodesHash={};//allow access to node position in O(1)
+	var edges=e || [];//type can be 'link','parent','posinfl','neginfl','rw_rule' (contain nodes id only)
+	var svg=s;
+	var width;
+	var	height;
+	var kami=k;
+	var force=null;
+	var first_init;
+	var container=c;//the html container
+	var svg=null;//the graphical caneva
+	this.init=function init(){
+		var width=document.getElementById(containerID).getBoundingClientRect().width;
+		var	height =document.getElementById(containerID).getBoundingClientRect().height-30;//menu is 30px heigth	
+		for(var i=0;i<nodes.length;i++)//add a hashtable for nodes.
+			nodesHash[nodes[i].id]=i;
+		force=d3.layout.force();
+		force.nodes(nodes)
+		.links(links)
+		.linkDistance(function(d){if(kami.getEdge(d.id).getType()!='parent') return 100; else return (nodeSize(kami.getNode(d.source).getType())+nodeSize(kami.getNode(d.target).getType()))})
+		.linkStrength(function(d){if(kami.getEdge(d.id).getType()!='parent') return 0.7; else return 5})
+		.charge(function(d){if(kami.getNode(d.id).getType()[0]=='action' && kami.getNode(d.id).getType()[1]!='input' && kami.getNode(d.id).getType()[1]!='output' ) return -300; else return -600})
+		.chargeDistance(function(d){
+			if((kami.getNode(d.id).getType()[0]=='action' && kami.getNode(d.id).getType()[1]!='input' && kami.getNode(d.id).getType()[1]!='output') || (kami.getNode(d.id).getType()[0]=='action' && kami.getNode(d.id).getType()[1]=='agent')) 
+				return 100;
+			else 
+				return nodeSize(kami.getNode(d.id).getType());
+			})
+		.size([width, height]);
+		first_init=false;
+		force.on("tick",tick);//add the tick function
+		force.drag().on("dragstart", dragstart);//add th dragging function
+		kami.setState("AGV");
+		svg=d3.select("#"+container).append("svg:svg")//create the SVG container
+			.attr("width",width)
+			.attr("height",height)
+			.on("contextmenu",d3.contextMenu(function(){return rightClickMenu();}));//add the right click menu
+		d3.select("#"+container).append("div")//add the top tooltype
+			.classed("n_tooltip",true)
+			.style("visibility","hidden");
+		d3.select("#"+container).append("div")//add the bottom tooltip
+			.classed("s_tooltip",true)
+			.style("visibility","hidden");
+		svg.append("svg:defs").selectAll("marker")//add the arrow at the end of edged
+		.data(["pos_end","neg_end","link_end","rw_end"])      // Different link/path types can be defined here
+		.enter().append("svg:marker")    // This section adds in the arrows
+		.attr("id", function(d){return d;})
+		.attr("refX", 40)
+		.attr("refY", 7)
+		.attr("markerWidth", 13)
+		.attr("markerHeight", 13)
+		.attr("orient", "auto")
+		.attr("markerUnits","strokeWidth")
+		.append("svg:path")
+		.attr("d", "M2,2 L2,13 L8,7 L2,2");
+	};
+	var nodeSize=function(type){//return the node RADIUS
+		//this node type : component(1)/action(2)/super(3)/attribute (1):agent/region/keyres/flag (2):modPos/modNeg/syn/deg/bnd/brk/input/output (3):family/set/process
+		if(type[0]=='attribute')
+			return 7;
+		switch(type[1]){
+			case 'flag':
+				return 7;
+			case "keyres":
+				return 10;
+			case "region":
+				return 15;
+			case "agent":
+			case "family":
+			case "set":
+			case "process":
+				return 20;
+			case "input":
+			case "output":
+				return 2;
+			case "modPos":
+			case "modNeg":
+			case "syn":
+			case "neg":
+			case "bnd":
+			case "brk":
+				return 40;
+			default:
+				throw "unknown type ! "+type;
+		}
+	}
+	this.wakeUp = function wakeUp(val){//speed up tick function
+		force.start();
+		if(!(typeof(val)!="undefined" && val!=null && !val))
+			d3.selectAll("g.agent").classed("fixed",false);
+			d3.selectAll("g.action").classed("fixed",false);
+		for(var i=0;i<300;i++){
+			force.tick();
+		}
+		if(first_init){
+			d3.selectAll("g.agent").classed("fixed",true);
+			d3.selectAll("g.action").classed("fixed",true);
+		}
+		first_init=true;
+	};
+	var update = function(){//update all the SVG elements
+		//links svg representation
+		edges = svg.selectAll(".edge")
+			.data(kami.getEdges(), function(d) { return d.id; });
+        edges.enter().insert("line","g")
+			.classed("edge",true)//type can be 'link','parent','posinfl','neginfl','rw_rule'
+            .classed("link",function(d){return kami.getType(d.id)=="link"})
+			.classed("parent",function(d){return kami.getType(d.id)=="parent"})
+			.classed("influence",function(d){return kami.getType(d.id)=="posinfl" || kami.getType(d.id)=="neginfl"})
+			.classed("posinfl",function(d){return kami.getType(d.id)=="posinfl"})
+			.classed("neginfl",function(d){return kami.getType(d.id)=="neginfl"})
+			.classed("rw_rule",function(d){return kami.getType(d.id)=="rw_rule"});
+		d3.selectAll(".link").on("contextmenu",d3.contextMenu(function(){return edgeCtMenu();}));
+		d3.selectAll(".influence").on("contextmenu",d3.contextMenu(function(){return edgeCtMenu();}));
+		d3.selectAll(".rw_rule").on("contextmenu",d3.contextMenu(function(){return edgeCtMenu();}));
+		d3.selectAll(".posinfl").attr("marker-end", "url(#pos_end)");
+		d3.selectAll(".neginfl").attr("marker-end", "url(#neg_end)");
+		d3.selectAll(".link").attr("marker-end", "url(#link_end)");
+		d3.selectAll(".rw_rule").attr("marker-end", "url(#rw_end)");
+        edges.exit().remove();
+		//none action nodes svg representation
+		nodes = svg.selectAll(".node")
+			  .data(kami.getNodes(), function(d) { return d.id;});//add all node id to the graphic structure
+		var nodeEnter = nodes.enter().insert("g")
+            .classed("node",true)//this node type : component(1)/action(2)/super(3)/attribute (1):agent/region/keyres/flag (2):modPos/modNeg/syn/deg/bnd/brk/input/output (3):family/set/process
+			.classed("round",function(d){return kami.getType(d.id)[0]=="attribute" || kami.getType(d.id)[0]=="component" || (kami.getType(d.id)[0]=="action" && (kami.getType(d.id)[1]=="input" || kami.getType(d.id)[1]=="output"));})
+			.classed("rectangle",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]!="input" && kami.getType(d.id)[1]!="output";})
+			.classed("attribute",function(d){return kami.getType(d.id)[0]=="attribute";})
+			.classed("component",function(d){return kami.getType(d.id)[0]=="component";})
+			.classed("agent",function(d){return kami.getType(d.id)[0]=="component" && kami.getType(d.id)[1]=="agent";})
+			.classed("keyres",function(d){return kami.getType(d.id)[0]=="component" && kami.getType(d.id)[1]=="keyres";})
+			.classed("flag",function(d){return kami.getType(d.id)[0]=="component" && kami.getType(d.id)[1]=="flag";})
+			.classed("region",function(d){return kami.getType(d.id)[0]=="component" && kami.getType(d.id)[1]=="region";})
+			.classed("action",function(d){return kami.getType(d.id)[0]=="action";})
+			.classed("modPos",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="modPos";})
+			.classed("modNeg",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="modNeg";})
+			.classed("syn",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="syn";})
+			.classed("deg",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="deg";})
+			.classed("bnd",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="bnd";})
+			.classed("brk",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="brk";})
+			.classed("input",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="input";})
+			.classed("output",function(d){return kami.getType(d.id)[0]=="action" && kami.getType(d.id)[1]=="output";})
+			.on("mouseover",mouseOver)
+			.on("mouseout",mouseOut)
+			.on("click",clickHandler)
+			.on("contextmenu",d3.contextMenu(function(){return nodeCtMenu();}));
+		//add dragging function
+		nodeEnter.selectAll("g.rectangle").call(drag);
+		nodeEnter.selectAll("g.rectangle").on("dblclick",clickNugget);
+		nodeEnter.selectAll("g.agent").call(drag);
+		//add actions (rectangle)
+		nodeEnter.selectAll("g.rectangle").insert("rect")
+			.attr("width", function(d){return nodeSize(kami.getType(d.id))})
+			.attr("height", function(d){return nodeSize(kami.getType(d.id))});
+		nodeEnter.selectAll("g.rectangle").insert("text")
+			.classed("nodeLabel",true)
+			.attr("x", function(d){return nodeSize(kami.getType(d.id));})
+			.attr("dy", function(d){return nodeSize(kami.getType(d.id))/2;})
+			.attr("text-anchor", "middle")
+			.text(function(d) {if(kami.getLabel(d.id).length>0) return kami.getLabel(d.id)[0]; else return d.id;})
+			.attr("font-size", function(d){ return nodeSize(kami.getType(d.id))/1.5+"px";});
+		//add components (circle)
+		nodeEnter.selectAll(".round").insert("circle")
+			.attr("r", function(d){return nodeSize(kami.getType(d.id));});
+		//add text.
+		nodeEnter.selectAll(".component").insert("text")
+			.classed("nodeLabel",true)
+			.attr("x", 0)
+			.attr("dy", ".35em")
+			.attr("text-anchor", "middle")
+			.text(function(d) {if(kami.getLabel(d.id).length>0) return kami.getLabel(d.id)[0]; else return d.id;})
+			.attr("font-size", function(d){return nodeSize(kami.getType(d.id))+"px";});
+		//remove old elements
+		nodes.exit().remove();
+		force.start();	
+	};
+	var tick = function(){//show up new svg element only if there position datas have been computed
+		if(first_init || force.alpha()<=0.00501){
+			//tick for nodes
+			nodes.attr("transform", function(d) {
+				if(kami.getType(d.id)[0]!="action" && kami.getType(d.id)[1]!="input" && kami.getType(d.id)[1]!="output"){
+					d.x=Math.max(nodeSize(kami.getType(d.id)), Math.min(width - nodeSize(kami.getType(d.id)), d.x));
+					d.y=Math.max(nodeSize(kami.getType(d.id)), Math.min(height - nodeSize(kami.getType(d.id)), d.y));
+					if()
+					return "translate(" + d.x + "," + d.y + ")"; 
+					if(kami.getType(d.id)[0]=="action"){
+						var in_out=kami.getSons(d.id);
+						var mult_in=false;
+						var mult_out=false;
+						for(var i=0;i<in_out.length;i++){
+							if(kami.getType(in_out[i])[0]!="attribute"){
+								if(kami.getType(in_out[i])[1]=="input" && mult_in){
+									
+							}
+						d.x=nodes[nodesHash[kami.getFather(d.id)]].x-nodeSize(kami.getType(kami.getFather(d.id)))
+						d.y=nodes[nodesHash[kami.getFather(d.id)]].y-(nodeSize(kami.getType(kami.getFather(d.id)))/2)
+					}
+				}
+			});
+			//tick for edges
+			edges.attr("x1", function(d){return nodes[nodesHash[d.source]].x;})
+				.attr("y1", function(d){return nodes[nodesHash[d.source]].y;})
+				.attr("x2", function(d){return nodes[nodesHash[d.target]].x;})
+				.attr("y2", function(d){return nodes[nodesHash[d.target]].y;});
+			first_init=true;
+		}
+	};
+}
+function Kami(container){//kami output : no access to node/edges, only there meta datas !
 	var DEFAULT_UID =0;//default uid counter (every node has a uid, protein have specific uid called up_id)
 	var NUGGET_ID = 0;//unique id for nuggets
 	var nugget_main_act=null;//nuggets main actions
@@ -563,8 +771,6 @@ function Kami(container){
 	var ng_undo_redo=null;//undo redo stack for nuggets
 	var ag_undo_redo=null;//undo redo stack for action graph
 	var graph_layout=null;//dynamic graph layout (force layout)
-	var graph_container=container;//the html container
-	var svg=null;//the graphical caneva
 	this.init = function init(){
 		DEFAULT_UID =0;
 		NUGGET_ID = 0;
@@ -587,12 +793,31 @@ function Kami(container){
 		update();
 	};
 	var setState = function(st){//set Kami to a specific state (this change the current layer and ifluence the element of the menu witch are shown)
+		if(currentView==st)
+			return;
+		switch(st){
+			case "NUV":
+			if(current_nugget ==-1)
+				currentLayer=nugget_graph;
+				
+			break;
+			case "AGV":
+			break;
+			case "LCG":
+			break;
+			case "KPV":
+			break;
+			default:
+			throw "unknow state !";
+		}
+	}
+	var initLGui = function(){
+		
+	};
+	var guiShow = function(){//generate if needed and show the current layerGraph
 		
 	}
-	var guiShow(){//generate if needed and show the current layerGraph
-		
-	}
-	var listenerInit(){//add all the listener to html elements !
+	var listenerInit = function(){//add all the listener to html elements !
 		
 	}
 	var update = function(delta){//core function that update the UI according to graph modification. if delta is defined, it will only update from delta, !!!! else, it will regenerate the full graph !!!! (time consuming process !);
@@ -609,7 +834,17 @@ function Kami(container){
 	
 } 
 
-
+/*
+TODO
+afficher graph
+gerer event
+switch de vue
+projection (from nugget to action graph)
+compilation (use only action graph) 
+traduction (use lcg+nugget)
+attention: un noeud de nugget peut au final correspondre à de multiple noeud dans le lcg !, il peut aussi avoir disparu, inversement un noeud du lcg peut correspondre à plusieur noeud dans les nuggets !
+regle Kappa : on prend les action du LCG et les noeuds du LCG et on regarde les nuggets qui les mentionnent.
+*/
 //notes : créer tout les flag en kappa avec un state default : ask user.
 //also ask for default rates.
 //penser que label des agent strictement differents ! si un label en commun alors c'est le meme agent !
